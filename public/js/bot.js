24e9d000130cff58123897e869062a8f645fcac1
sam <sam.ternent@gmail.com>
2013-11-17 01:38:21 +0100

    refactor and extra functionality
    game loop
    new movement logic

--- public/js/bot.js
+++ public/js/bot.js
@@ -1,3 +1,10 @@
+ // gameObj
+
+var Game = {
+    active: false,
+    liveBots: 0
+};
+
 //script for the bot program
 window.onload = function () {
 
@@ -10,69 +17,108 @@ window.onload = function () {
         enemyBot4DOM = document.getElementById('enemyBot4');
 
     var samSettings = {
-        speedX: 7,
-        speedY: 15,
-        startPosX: 0,
-        startPosY: 0,
-        visionLength: 150,
-        visionWidth: 50
+        speed: 17,
+        startPos: {
+            x: 0,
+            y: 0
+        },
+        vision: {
+            length: 200,
+            width: 50
+        },
+        angle: 45,
+        health: 100
     },
     enemy1Settings = {
-        speedX: 20,
-        speedY: 13,
-        startPosX: 400,
-        startPosY: 0,
-        visionLength: 100,
-        visionWidth: 50
+        speed: 13,
+        startPos: {
+            x: 100,
+            y: 100
+        },
+        vision: {
+            length: 70,
+            width: 50
+        },
+        health: 100
     },
     enemy2Settings = {
-        speedX: 5,
-        speedY: 19,
-        startPosX: 0,
-        startPosY: 400,
-        visionLength: 50,
-        visionWidth: 50
+        speed: 9,
+        startPos: {
+            x: 20,
+            y: 300
+        },
+        vision: {
+            length: 130,
+            width: 50
+        },
+        angle: 90,
+        health: 100
     },
     enemy3Settings = {
-        speedX: 18,
-        speedY: 9,
-        startPosX: 400,
-        startPosY: 400,
-        visionLength: 40,
-        visionWidth: 50
+        speed: 20,
+        startPos: {
+            x: 400,
+            y: 400
+        },
+        vision: {
+            length: 100,
+            width: 50
+        },
+        angle: 76,
+        health: 100
     },
     enemy4Settings = {
-        speedX: 8,
-        speedY: 13,
-        startPosX: 300,
-        startPosY: 200,
-        visionLength: 140,
-        visionWidth: 50
+        speed: 11,
+        startPos: {
+            x: 30,
+            y: 400
+        },
+        vision: {
+            length: 50,
+            width: 50
+        },
+        angle: 240,
+        health: 100
     };
 
 
     // create bot instances
-    var samBot = Bot(samBotDOM,stage,samSettings),
-        enemyBot1 = Bot(enemyBot1DOM,stage,enemy1Settings),
-        enemyBot2 = Bot(enemyBot2DOM,stage,enemy2Settings),
-        enemyBot3 = Bot(enemyBot3DOM,stage,enemy3Settings),
-        enemyBot4 = Bot(enemyBot4DOM,stage,enemy4Settings);
+    var gameBots = [Bot(samBotDOM,stage,samSettings),
+        Bot(enemyBot1DOM,stage,enemy1Settings),
+        Bot(enemyBot2DOM,stage,enemy2Settings),
+        Bot(enemyBot3DOM,stage,enemy3Settings),
+        Bot(enemyBot4DOM,stage,enemy4Settings)];
 
     // initialize bots
-    samBot.init();
-    enemyBot1.init();
-    enemyBot2.init();
-    enemyBot3.init();
-    enemyBot4.init();
+    for (var i=0; i < gameBots.length; i++) {
+        gameBots[i].init();
+    }
+
+    Game.liveBots = gameBots.length;
+    Game.active = true;
 
     // start the loop
-    setInterval(function () {
-        samBot.go();
-        enemyBot1.go();
-        enemyBot2.go();
-        enemyBot3.go();
-        enemyBot4.go();
-    }, 100);
+
+    if (Game.active) {
+        var gameLoop = setInterval(function () {
+            for (var i=0; i < gameBots.length; i++) {
+                gameBots[i].go();
+            }
+            if (Game.liveBots < 2) {
+                Game.active = false;
+                clearGameLoop();
+            }
+        }, 100);
+
+        var clearGameLoop = function () {
+            clearInterval(gameLoop);
+            gameOver();
+        }
+    }
+}
+
+var gameOver = function () {
+    alert('Game over!');
 }
 
 
@@ -80,16 +126,24 @@ window.onload = function () {
 var Bot = function (myBot,stage,settings) {
 
     // bot settings
-    var speedX = settings.speedX,
-        speedY = settings.speedY,
-        speed = 10,
-        angle = 45,
-        direction = 0,
+    var speed = settings.speed,
+        angle = settings.angle,
+        velocity = {
+            x: 0,
+            y: 0
+        },
+        direction = {
+            x: 1,
+            y: 1
+        },
+        health = settings.health,
         bots = [],
+        tracking = false,
+        alive = true,
         vision = function (obj) {
             var vision =  myBot.getElementsByClassName('vision')[0];
-            vision.style.width = settings.visionLength + 'px';
-            vision.style.height = settings.visionWidth + 'px';
+            vision.style.width = settings.vision.length + 'px';
+            vision.style.height = settings.vision.width + 'px';
             return vision;
         },
         bounding = function (obj) {
@@ -98,18 +152,30 @@ var Bot = function (myBot,stage,settings) {
 
         // main loop
         go = function () {
-            var scaleX = Math.cos(angle),
-                scaleY = Math.sin(angle),
-                velocityX = speed*scaleX,
-                velocityY = speed*scaleY,
-                topPos = parseInt(myBot.style.top),
-                leftPos = parseInt(myBot.style.left);
 
-            myBot.style.top = topPos + velocityX + 'px';
-            myBot.style.left = leftPos + velocityY + 'px'
+            if (alive) {
+                var scaleX = Math.cos(angle),
+                    scaleY = Math.sin(angle),
+                    topPos = parseInt(myBot.style.top),
+                    leftPos = parseInt(myBot.style.left);
 
-            checkForBot();
-            checkForWalls();
+                velocity.x = speed*direction.x;
+                velocity.y = speed*direction.y;
+
+                myBot.style.top = topPos+velocity.y + 'px';
+                myBot.style.left = leftPos+velocity.x + 'px'
+
+                checkForBot();
+                checkForWalls();
+
+                if (health < 30) {
+                    myBot.style.boxShadow = '0 0 10px 5px rgba(255,0,0,10)'
+                } if (health < 0) {
+                    killBot();
+                }
+            } else {
+                killBot();
+            }
         },
 
         // stage collision detection
@@ -118,16 +184,23 @@ var Bot = function (myBot,stage,settings) {
                 stageBox = bounding(stage);
 
 
-            if (botBox.bottom > stageBox.bottom &&speed > 0 ||
-                botBox.top < stageBox.top && speed < 0 ||
-                botBox.right > stageBox.right && speed > 0 ||
-                botBox.left < stageBox.left && speed < 0) {
-                speed *= -1;
+            if (botBox.bottom > stageBox.bottom && direction.y > 0 ||
+                botBox.top < stageBox.top && direction.y < 0) {
+                direction.y *= -1;
                 vision().style.backgroundColor = 'orange';
+                health -= 5;
             }
-            //angle = Math.atan2(speedY, speedX)/(Math.PI/180);
+            if (botBox.right > stageBox.right && direction.x > 0 ||
+                botBox.left < stageBox.left && direction.x < 0) {
+                direction.x *= -1;
+                vision().style.backgroundColor = 'orange';
+                health -= 5;
+            }
 
-            //myBot.style.webkitTransform = 'rotate(' + angle + 'deg)';
+            if (!tracking) {
+                angle = Math.atan2(velocity.y, velocity.x)/(Math.PI/180);
+            }
+            myBot.style.webkitTransform = 'rotate(' + angle + 'deg)';
         },
 
 
@@ -143,7 +216,6 @@ var Bot = function (myBot,stage,settings) {
             var hits = [];
             for (var i=0;i<bots.length;i++) {
                 var enemyBox = bounding(bots[i]);
-                //var distance = getDistance(botBox.right, botBox.top+(botBox.height/2), enemyBox.left+(enemyBox.width/2), enemyBox.top+(enemyBox.height/2));
 
                 if(bots[i].id !== myBot.id) {
                     if (enemyBox.right < botBox.left ||
@@ -155,13 +227,27 @@ var Bot = function (myBot,stage,settings) {
                     }
                 }
             }
-            if(hits.length > 0) {
+            if (hits.length > 0) {
+                tracking = true;
                 vision().style.backgroundColor = 'red';
+                angle = Math.atan2(hits[0].top, hits[0].left)/(Math.PI/180);
             } else {
+                tracking = false;
                 vision().style.backgroundColor = 'transparent';
             }
         },
 
+        // kill the bot
+        killBot = function () {
+            if (alive) {
+                Game.liveBots -= 1;
+            }
+            alive = false;
+            vision().style.backgroundColor = 'transparent';
+            myBot.style.top = '105%';
+            myBot.style.position = 'relative';
+            myBot.style.float = 'left';
+        },
         // get other bots
         getOtherBots = function () {
             return stage.getElementsByClassName('bot');
@@ -169,8 +255,8 @@ var Bot = function (myBot,stage,settings) {
 
         // initialize
         init = function () {
-            myBot.style.top = settings.startPosY + 'px';
-            myBot.style.left = settings.startPosX + 'px';
+            myBot.style.top = settings.startPos.y + 'px';
+            myBot.style.left = settings.startPos.x + 'px';
             bots = getOtherBots();
         };
 
